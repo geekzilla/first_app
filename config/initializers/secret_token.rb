02d2338f17422ebfb1a68bcd6a4d20d7a73748ae
RailZilla <RailZilla@eyeglu.com>
2013-10-03 22:28:18 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e56f2fbbaecab206590dc7caf8a300764efc563aebc53aec1d6521e19468f058cdb24df7c5cebb33fe261886d1b80e80534c23fd24e31b371ccd66270638f8fe'
